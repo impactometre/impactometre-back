bb62dfb661ed87cf5ee7e7ad2040d1cabdaa0ab6
cbrizard <clement.brizard@etu.utc.fr>
2019-12-21 17:53:10 +0100

    feat: add software values

--- model/database/meeting-equipment.js
+++ model/database/meeting-equipment.js
@@ -264,6 +264,43 @@ export const meetingEquipment = {
       resources: Math.pow(1.18, 3)
     }
   },
+  RENAVISIO: {
+    french: 'Renavisio',
+    // in Mo
+    fileSize: 18.8,
+    // in Kbits/s
+    downloadSpeed: 1080
+  },
+  SKYPE: {
+    french: 'Skype',
+    fileSize: 65.8,
+    // Indexed by number of participants
+    // Source : https://support.skype.com/fr/faq/FA1417/quelle-est-la-quantite-de-bande-passante-necessaire-a-skype
+    downloadSpeed: {
+      2: {
+        lower: 30,
+        upper: 1050
+      },
+      3: {
+        lower: 512,
+        upper: 2000
+      },
+      5: {
+        lower: 2000,
+        upper: 4000
+      },
+      // 7 or more
+      7: {
+        lower: 4000,
+        upper: 8000
+      }
+    }
+  },
+  JITSI: {
+    french: 'Jitsi',
+    fileSize: 36.21,
+    downloadSpeed: 'unknown'
+  },
   PLANE_INTERCONTINENTAL_ONE_PERSON_KM: {
     french: 'avion intercontinental',
     embodied: {
