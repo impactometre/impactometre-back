7a7b30c83e44750b60c76a84eb5a2e9f7472bbb0
cbrizard <clement.brizard@etu.utc.fr>
2019-12-25 10:59:27 +0100

    fix: rename getDownloadSpeed by getInboundBandwith

--- model/services/meeting/software.js
+++ model/services/meeting/software.js
@@ -10,45 +10,45 @@ function get (name) {
  * Return the given software download speed.
  * @param {String} softwareName - The software name.
  * @param {Number} participantsNumber - The participants number.
- * @param {String} bound - The bound ('upper' or 'lower').
+ * @param {String} bound - The bound ('minimum' or 'ideal').
  */
-function getDownloadSpeedOf (softwareName, participantsNumber, bound) {
+function getInboundBandwith (softwareName, participantsNumber, bound) {
   const software = get(softwareName)
-  const rawDownloadSpeed = software.downloadSpeed
+  const rawInbound = software.bandwith.inbound
 
   /* If we don't have data specific to a number of
   participants, we return the unique value we got */
-  if (typeof rawDownloadSpeed === 'number') {
-    return rawDownloadSpeed
+  if (typeof rawInbound === 'number') {
+    return rawInbound
   }
 
   /* Among the available download speed values, we get the
   one which participants number is the closest from the
   given participants number.
   */
-  const availableNumbers = Object.keys(rawDownloadSpeed)
+  const availableNumbers = Object.keys(rawInbound)
   const closestAvailableNumber = getClosest(
     participantsNumber,
     availableNumbers
   )
 
-  const closestDownloadSpeed = software.downloadSpeed[closestAvailableNumber]
+  const closestValue = software.bandwith.inbound[closestAvailableNumber]
 
   /* If we don't have bound specific data,
   we return the unique value we got */
-  if (typeof closestDownloadSpeed === 'number') {
-    return closestDownloadSpeed
+  if (typeof closestValue === 'number') {
+    return closestValue
   }
 
   /* If desired bound value was given, we return
   the corresponding value. Else we return the
-  upper value.
+  ideal value.
   */
-  const boundSpecificDownloadSpeed = (bound != null)
-    ? closestDownloadSpeed[bound]
-    : closestDownloadSpeed.upper
+  const boundSpecificValue = (bound != null)
+    ? closestValue[bound]
+    : closestValue.ideal
 
-  return boundSpecificDownloadSpeed
+  return boundSpecificValue
 }
 
-module.exports = { get, getDownloadSpeedOf }
+module.exports = { get, getInboundBandwith }
