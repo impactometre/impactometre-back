dc0ac32bfbae39d7841eb3c2b88dc84d7e1b6301
cbrizard <clement.brizard@etu.utc.fr>
2019-12-25 12:28:41 +0100

    feat: move software service functions to new softawre class

---
+++ model/classes/Software.js
@@ -0,0 +1,57 @@
+'use strict'
+
+const getClosest = require('../../utils/get-closest')
+
+class Software {
+  constructor (software) {
+    this.french = software.french
+    this.fileSize = software.fileSize
+    this.bandwith = software.bandwith
+  }
+
+  /**
+ * Return the given software download speed.
+ * @param {String} softwareName - The software name.
+ * @param {Number} participantsNumber - The participants number.
+ * @param {String} bound - The bound ('minimum' or 'ideal').
+ */
+  getInboundBandwith (participantsNumber, bound) {
+    const rawInbound = this.bandwith.inbound
+
+    /* If we don't have data specific to a number of
+    participants, we return the unique value we got */
+    if (typeof rawInbound === 'number') {
+      return rawInbound
+    }
+
+    /* Among the available download speed values, we get the
+    one which participants number is the closest from the
+    given participants number.
+    */
+    const availableNumbers = Object.keys(rawInbound)
+    const closestAvailableNumber = getClosest(
+      participantsNumber,
+      availableNumbers
+    )
+
+    const closestValue = this.bandwith.inbound[closestAvailableNumber]
+
+    /* If we don't have bound specific data,
+    we return the unique value we got */
+    if (typeof closestValue === 'number') {
+      return closestValue
+    }
+
+    /* If desired bound value was given, we return
+    the corresponding value. Else we return the
+    ideal value.
+    */
+    const boundSpecificValue = (bound != null)
+      ? closestValue[bound]
+      : closestValue.ideal
+
+    return boundSpecificValue
+  }
+}
+
+module.exports = Software
