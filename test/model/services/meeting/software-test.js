9fba567a3ded539437eadad738a05090b2698f20
clementbrizard <clement.brizard@mailo.com>
2019-12-25 12:28:41 +0100

    feat: move software service functions to new softawre class

--- test/model/services/meeting/software-test.js
+++ test/model/services/meeting/software-test.js
@@ -1,65 +1,52 @@
 'use strict'
 
 const assert = require('assert')
-const Software = require('../../../../model/services/meeting/software')
+// const Software = require('../../../../model/services/meeting/software')
+const softwareDatabase = require('../../../../model/database/meeting/software')
+const Software = require('../../../../model/classes/Software')
 
-describe('Software data access', () => {
-  describe('#get(name)', () => {
-    it('should return the software corresponding to the given name', () => {
-      assert.notStrictEqual(
-        Software.get('RENAVISIO'),
-        {
-          french: 'Renavisio',
-          fileSize: 18.8,
-          bandwith: {
-            inbound: 1080
-          }
-        }
-      )
-    })
+describe('#getInboundBandwith()', () => {
+  const renavisio = new Software(softwareDatabase.RENAVISIO)
+  it('should return the unique available value', () => {
+    assert.strictEqual(
+      renavisio.getInboundBandwith('RENAVISIO'),
+      1080
+    )
   })
 
-  describe('#getInboundBandwith()', () => {
-    it('should return the unique available value', () => {
-      assert.strictEqual(
-        Software.getInboundBandwith('RENAVISIO'),
-        1080
-      )
-    })
+  it('should return the unique available value even if number of participants specified', () => {
+    assert.strictEqual(
+      renavisio.getInboundBandwith(3),
+      1080
+    )
+  })
 
-    it('should return the unique available value even if number of participants specified', () => {
-      assert.strictEqual(
-        Software.getInboundBandwith('RENAVISIO', 3),
-        1080
-      )
-    })
+  const skype = new Software(softwareDatabase.SKYPE)
+  it('should return the matching available (minimum bound) value', () => {
+    assert.strictEqual(
+      skype.getInboundBandwith(2, 'minimum'),
+      30
+    )
+  })
 
-    it('should return the matching available (minimum bound) value', () => {
-      assert.strictEqual(
-        Software.getInboundBandwith('SKYPE', 2, 'minimum'),
-        30
-      )
-    })
+  it('should return the matching available ideal bound value when no bound specified', () => {
+    assert.strictEqual(
+      skype.getInboundBandwith(2),
+      1050
+    )
+  })
 
-    it('should return the matching available ideal bound value when no bound specified', () => {
-      assert.strictEqual(
-        Software.getInboundBandwith('SKYPE', 2),
-        1050
-      )
-    })
+  it('should return the closest greater available (ideal bound) value', () => {
+    assert.strictEqual(
+      skype.getInboundBandwith(4, 'ideal'),
+      4000
+    )
+  })
 
-    it('should return the closest greater available (ideal bound) value', () => {
-      assert.strictEqual(
-        Software.getInboundBandwith('SKYPE', 4, 'ideal'),
-        4000
-      )
-    })
-
-    it('should return the last available value if participants number is greater than available values', () => {
-      assert.strictEqual(
-        Software.getInboundBandwith('SKYPE', 8, 'ideal'),
-        8000
-      )
-    })
+  it('should return the last available value if participants number is greater than available values', () => {
+    assert.strictEqual(
+      skype.getInboundBandwith(8, 'ideal'),
+      8000
+    )
   })
 })
