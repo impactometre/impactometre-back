e89cd2b3acb0f628946e049a070c87a69372c221
ValLG <valentin.legauche@laposte.net>
2019-12-26 17:54:37 +0100

    feat: create SoftwareEntered class

---
+++ model/classes/SoftwareEntered.js
@@ -0,0 +1,17 @@
+'use strict'
+
+/**
+ * This class discribed a kind of software device
+ * entered by the user, with its number of instances used for the meetings.
+ */
+export default class SoftwareEntered {
+  /**
+   * The SoftwareEntered class constructor.
+   * @param {software} software The kind of software used.
+   * @param {Integer} numberOfInstances Its number of instances entered by the user.
+   */
+  constructor (software, numberOfInstances) {
+    this.software = software
+    this.numberOfInstances = numberOfInstances
+  }
+}
