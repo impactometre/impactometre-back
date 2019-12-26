3d6257d983aa564077f78e421bfcca8e173dad1b
ValLG <valentin.legauche@laposte.net>
2019-12-26 17:53:30 +0100

    feat: create HardwareEntered class

---
+++ model/classes/HardwareEntered.js
@@ -0,0 +1,17 @@
+'use strict'
+
+/**
+ * This class discribed a kind of hardware device
+ * entered by the user, with its number of instances used for the meetings.
+ */
+export default class HardwareEntered {
+  /**
+   * The HardwareEntered class constructor.
+   * @param {hardware} hardware The kind of hardware device.
+   * @param {Integer} numberOfInstances Its number of instances entered by the user
+   */
+  constructor (hardware, numberOfInstances) {
+    this.hardware = hardware
+    this.numberOfInstances = numberOfInstances
+  }
+}
