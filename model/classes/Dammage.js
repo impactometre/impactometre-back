2e6495bff4d2edbff90af0b5da873261e005bf35
clementbrizard <clement.brizard@mailo.com>
2019-12-27 17:09:47 +0100

    fix: correct JSdoc

--- model/classes/Dammage.js
+++ model/classes/Dammage.js
@@ -3,10 +3,10 @@
 export default class Dammage {
   /**
    * Create an object that represents the dammage of any element.
-   * @param {Number} humanHealth The damage value on Human Health.
-   * @param {Number} ecosystemQuality The damage value on Ecosystem Quality.
-   * @param {Number} climateChange The damage value on Climate Change.
-   * @param {Number} resources The damage value on Resources.
+   * @param {Number} humanHealth - The damage value on Human Health.
+   * @param {Number} ecosystemQuality - The damage value on Ecosystem Quality.
+   * @param {Number} climateChange - The damage value on Climate Change.
+   * @param {Number} resources - The damage value on Resources.
    */
   constructor (humanHealth, ecosystemQuality, climateChange, resources) {
     this.humanHealth = humanHealth
