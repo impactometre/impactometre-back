e48357e826f8399577f88ea5fa339e455ba046ff
clementbrizard <clement.brizard@mailo.com>
2019-12-28 22:47:45 +0100

    feat: separate network from hardware in database

--- model/database/meeting/hardware.js
+++ model/database/meeting/hardware.js
@@ -287,7 +287,7 @@ const hardware = {
     lifetime: 690
   },
   /**
-   * NETWORK
+   * NETWORK ACCESS
    */
   INTERNET_ACCESS_EQUIPMENT: {
     french: "équipement d'accès Internet",
@@ -300,27 +300,6 @@ const hardware = {
     // Same as laptop, considering a 5-year
     // student curriculum.
     lifetime: 8050
-  },
-  NETWORK_ENERGETIC_INTENSITY_LOWER: {
-    french: 'intensité énergétique du réseau (estimation basse)',
-    operatingOneBit: {
-      humanHealth: Math.pow(1.68, -17),
-      ecosystemQuality: Math.pow(7.76, -12),
-      climateChange: Math.pow(1.34, -11),
-      resources: Math.pow(3.47, -9)
-    },
-    // TODO : convert from electric consumption ?
-    lifetime: 'unknown'
-  },
-  NETWORK_ENERGETIC_INTENSITY_UPPER: {
-    french: 'intensité énergétique du réseau (estimation haute)',
-    operatingOneBit: {
-      humanHealth: Math.pow(2.8, -17),
-      ecosystemQuality: Math.pow(1.29, -11),
-      climateChange: Math.pow(2.23, -11),
-      resources: Math.pow(5.77, -9)
-    },
-    lifetime: 'unknown'
   }
 }
 
