630183201b39e5b052e491e70ad4db72eae40023
vlegauch <valentin.le-gauche@etu.utc.fr>
2019-12-28 16:53:28 +0100

    feat: add getters and setters to Journey class

--- model/classes/Journey.js
+++ model/classes/Journey.js
@@ -19,6 +19,62 @@ export default class Journey {
     this.distance = distance
     this.numberOfPeople = numberOfPeople
   }
+
+  // Getters
+
+  /**
+   * Getter of the journey id.
+   */
+  get id () {
+    return this.id
+  }
+
+  /**
+   * Getter of the journey transportation mean.
+   */
+  get mean () {
+    return this.mean
+  }
+
+  /**
+   * Getter of the journey distance.
+   */
+  get distance () {
+    return this.distance
+  }
+
+  /**
+   * Getter of the journey number of people.
+   */
+  get numberOfPeople () {
+    return this.numberOfPeople
+  }
+
+  // Setters
+
+  /**
+   * Setter of the journey transportation mean.
+   * @param {TransportationMean} transportationMean - The new journey transportation mean.
+   */
+  set mean (transportationMean) {
+    this.mean = transportationMean
+  }
+
+  /**
+   * Setter of the journey distance.
+   * @param {Float} distance - The new journey distance.
+   */
+  set distance (distance) {
+    this.distance = distance
+  }
+
+  /**
+   * Setter of the journey number of people.
+   * @param {Integer} numberOfPeople - The new journey number of people.
+   */
+  set numberOfPeople (numberOfPeople) {
+    this.numberOfPeople = numberOfPeople
+  }
 }
 
 module.exports = Journey
