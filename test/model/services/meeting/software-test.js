399b658923c209c295005e721bcbeea0a3ba7219
vlegauch <valentin.le-gauche@etu.utc.fr>
2019-12-31 11:17:21 +0100

    fix: repacle 'computes' by 'compute'

--- test/model/services/meeting/software-test.js
+++ test/model/services/meeting/software-test.js
@@ -53,7 +53,7 @@ describe('Software services', () => {
       )
     })
   })
-  describe('#computesEmbodiedDamage (instancesNumber, networkBound)', () => {
+  describe('#computeEmbodiedDamage (instancesNumber, networkBound)', () => {
     const renavisio = new Software(softwareDatabase.RENAVISIO)
     const networkEnergeticIntensityUpper = networkDatabase.NETWORK_ENERGETIC_INTENSITY_UPPER.operatingOneBit
     const fileSizeInBits = renavisio.fileSizeInBits()
@@ -65,7 +65,7 @@ describe('Software services', () => {
     )
     it('should return the solftware download damage for 5 people with Renaviso', () => {
       assert.deepStrictEqual(
-        renavisio.computesEmbodiedDamage(5, constants.networkEnergeticIntensityBound.UPPER),
+        renavisio.computeEmbodiedDamage(5, constants.networkEnergeticIntensityBound.UPPER),
         embodiedDamage
       )
     })
@@ -73,25 +73,25 @@ describe('Software services', () => {
     const emptyDamage = new ComponentDamage(0, 0, 0, 0)
     it('should return an empty damage because Jitsi has any file to download', () => {
       assert.deepStrictEqual(
-        jitsi.computesEmbodiedDamage(5, networkEnergeticIntensityUpper),
+        jitsi.computeEmbodiedDamage(5, networkEnergeticIntensityUpper),
         emptyDamage
       )
     })
     it('should return 0 because the number of people doesn\'t matter when there no file to download', () => {
       assert.deepStrictEqual(
-        jitsi.computesEmbodiedDamage(5, networkEnergeticIntensityUpper),
-        jitsi.computesEmbodiedDamage(10, networkEnergeticIntensityUpper)
+        jitsi.computeEmbodiedDamage(5, networkEnergeticIntensityUpper),
+        jitsi.computeEmbodiedDamage(10, networkEnergeticIntensityUpper)
       )
     })
     const networkEnergeticIntensityLower = constants.networkEnergeticIntensityBound.LOWER
     it('should return 0 because the network energetic intensity value doesn\'t matter when there no file to download', () => {
       assert.deepStrictEqual(
-        jitsi.computesEmbodiedDamage(5, networkEnergeticIntensityLower),
-        jitsi.computesEmbodiedDamage(10, networkEnergeticIntensityLower)
+        jitsi.computeEmbodiedDamage(5, networkEnergeticIntensityLower),
+        jitsi.computeEmbodiedDamage(10, networkEnergeticIntensityLower)
       )
     })
   })
-  describe('#computesOperatingDamage (instancesNumber, bandwithBound, networkBound)', () => {
+  describe('#computeOperatingDamage (instancesNumber, bandwithBound, networkBound)', () => {
     const skype = new Software(softwareDatabase.SKYPE)
     const inboundBandwith = skype.getInboundBandwith(5)
     const networkUpperkBound = networkDatabase.NETWORK_ENERGETIC_INTENSITY_UPPER.operatingOneBit
@@ -104,7 +104,7 @@ describe('Software services', () => {
     )
     it('should return skype operating damage for network upper bound and ideal bandwith', () => {
       assert.deepStrictEqual(
-        skype.computesOperatingDamage(instancesNumber, constants.bandwidthBound.IDEAL, constants.networkEnergeticIntensityBound.UPPER),
+        skype.computeOperatingDamage(instancesNumber, constants.bandwidthBound.IDEAL, constants.networkEnergeticIntensityBound.UPPER),
         operatingDamage
       )
     })
@@ -118,7 +118,7 @@ describe('Software services', () => {
     )
     it('should return skype operating damage for network lower bound and minimun bandwith', () => {
       assert.deepStrictEqual(
-        skype.computesOperatingDamage(instancesNumber, constants.bandwidthBound.MINIMUM, constants.networkEnergeticIntensityBound.LOWER),
+        skype.computeOperatingDamage(instancesNumber, constants.bandwidthBound.MINIMUM, constants.networkEnergeticIntensityBound.LOWER),
         operatingDamageMinimumBandwith
       )
     })
