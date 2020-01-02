4d256ab45fcb736da2aac4792be5f4e12ad22281
vlegauch <valentin.le-gauche@etu.utc.fr>
2020-01-02 14:37:55 +0100

    refactor: modify the war that network energetic intensities are stored in database

--- model/database/meeting/network.js
+++ model/database/meeting/network.js
@@ -1,26 +1,20 @@
 'use strict'
 
 const network = {
-  NETWORK_ENERGETIC_INTENSITY_LOWER: {
-    french: 'intensité énergétique du réseau (estimation basse)',
-    operatingOneBit: {
+  NETWORK_ENERGETIC_INTENSITY: {
+    french: 'intensité énergétique du réseau',
+    operatingOneBitLower: {
       humanHealth: Math.pow(1.68, -17),
       ecosystemQuality: Math.pow(7.76, -12),
       climateChange: Math.pow(1.34, -11),
       resources: Math.pow(3.47, -9)
     },
-    // TODO : convert from electric consumption ?
-    lifetime: 'unknown'
-  },
-  NETWORK_ENERGETIC_INTENSITY_UPPER: {
-    french: 'intensité énergétique du réseau (estimation haute)',
-    operatingOneBit: {
+    operatingOneBitUpper: {
       humanHealth: Math.pow(2.8, -17),
       ecosystemQuality: Math.pow(1.29, -11),
       climateChange: Math.pow(2.23, -11),
       resources: Math.pow(5.77, -9)
-    },
-    lifetime: 'unknown'
+    }
   }
 }
 
