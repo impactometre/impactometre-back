c7af11c6731012fa0a269fa688416e33e37559f9
Aurélien Béranger <aurelien.beranger@etu.utc.fr>
2020-01-27 18:00:08 +0100

    feat: mange journeys for a traveller

--- controllers/routes/index.js
+++ controllers/routes/index.js
@@ -3,13 +3,15 @@
 const express = require('express')
 const router = express.Router()
 
+const transportationMean = require('../../database/meeting/transportationMean')
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Impactometre' })
 })
 
 router.get('/reunion', function (req, res, next) {
-  res.render('meeting/form', { title: 'Votre réunion' })
+  res.render('meeting/form/form', { title: 'Votre réunion', transportationMean })
 })
 
 module.exports = router
