f6d5569b07b7d159ebda5222bf6b136d91a0cd08
Aurélien Béranger <aurelien.beranger@etu.utc.fr>
2020-01-28 00:10:40 +0100

    feat: meeting routing

--- controllers/routes/meeting/index.js
+++ controllers/routes/meeting/index.js
@@ -5,8 +5,14 @@ const resultsRouter = require('./results')
 
 const router = express.Router()
 
+const transportationMean = require('../../../database/meeting/transportationMean')
+
 router.get('/', function (req, res, next) {
-  res.redirect('/reunion/resultats')
+  res.redirect('/reunion/commencer')
+})
+
+router.get('/commencer', function (req, res, next) {
+  res.render('meeting/form/form', { title: 'Votre réunion', transportationMean })
 })
 
 router.use('/resultats', resultsRouter)
