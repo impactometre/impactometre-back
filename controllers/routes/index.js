6b9dd0b38468bd51bd5202e1eb6a37abf360ebeb
auberanger <aurelien.beranger@lilo.org>
2020-01-28 00:10:40 +0100

    feat: meeting routing

--- controllers/routes/index.js
+++ controllers/routes/index.js
@@ -3,15 +3,9 @@
 const express = require('express')
 const router = express.Router()
 
-const transportationMean = require('../../database/meeting/transportationMean')
-
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Impactometre' })
-})
-
-router.get('/reunion', function (req, res, next) {
-  res.render('meeting/form/form', { title: 'Votre réunion', transportationMean })
 })
 
 module.exports = router
