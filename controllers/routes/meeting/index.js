58602b0cfaa686b34c8b5bdb9096a7ea99dbfc92
Aurélien Béranger <aurelien.beranger@etu.utc.fr>
2020-01-28 02:49:23 +0100

    feat: build payload for software and hardware

--- controllers/routes/meeting/index.js
+++ controllers/routes/meeting/index.js
@@ -5,14 +5,25 @@ const resultsRouter = require('./results')
 
 const router = express.Router()
 
-const transportationMean = require('../../../database/meeting/transportationMean')
+const hardwareDb = require('../../../database/meeting/hardware')
+const softwareDb = require('../../../database/meeting/software')
+const transportationMeanDb = require('../../../database/meeting/transportationMean')
 
 router.get('/', function (req, res, next) {
   res.redirect('/reunion/commencer')
 })
 
 router.get('/commencer', function (req, res, next) {
-  res.render('meeting/form/form', { title: 'Votre réunion', transportationMean })
+  res.render('meeting/form/form', { title: 'Votre réunion', hardwareDb, softwareDb, transportationMeanDb })
+})
+
+router.post('/creer', (req, res) => {
+  console.log(req.body)
+  // The user who creates the meeting
+  const user = 'vlegauch'
+  // The meeting duration in minutes
+  const { meetingDuration, softwareChoice } = req.body
+  console.log(user, meetingDuration, softwareChoice)
 })
 
 router.use('/resultats', resultsRouter)
