6c5d48404b2c08e6c249c8aa60a40289e194938e
clementbrizard <clement.brizard@mailo.com>
2020-04-19 21:19:14 +0200

    feat: add Jitsi filesize

--- database/meeting/software.js
+++ database/meeting/software.js
@@ -46,15 +46,20 @@ const software = {
     }
   },
   JITSI: {
+    name: 'JITSI',
+    french: 'Jitsi',
+    category: meetingComponents.SOFTWARE,
+    /* Obtained summing size of all requests results when
+    we load a launched Jitsi visioconference. Mean realized
+    on visioconferences with 2, 3, 4 and 5 people.
+    */
+    fileSize: 2.04,
     /* Measured for 3 people
     - 1 000 000 kBytes received by all people for 15 minutes
     - 66 666 kBystes received each minute by all people
     - 8 888 kbits received each seconde by all people
     - 2963 kbits received each second by each people
     */
-    name: 'JITSI',
-    french: 'Jitsi',
-    category: meetingComponents.SOFTWARE,
     bandwith: {
       inbound: 2963
     }
