6a4e1764cf3505a497546a7df1499fa6a35c194b
Matt Glorion <matt@glorion.fr>
2020-06-23 14:52:24 +0200

    init + eslint

--- app.js
+++ app.js
@@ -4,38 +4,19 @@ const createError = require('http-errors');
 const path = require('path');
 
 const express = require('express');
-const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const app = express();
 
-const indexRouter = require('./controllers/routes');
-const meetingRouter = require('./controllers/routes/meeting');
-
-app.use(cookieParser());
+const apiRouter = require('./controllers/routes/api');
+// const indexRouter = require('./controllers/routes');
+// const meetingRouter = require('./controllers/routes/meeting');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/', indexRouter);
-app.use('/reunion', meetingRouter);
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+app.use(express.static(path.join(__dirname, 'public')))
+app.use('/api', apiRouter);
 
 module.exports = app;
