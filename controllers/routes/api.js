d78e40d0a7715de5b266183748f5639578ed83a4
Matt Glorion <matt@glorion.fr>
2020-06-23 14:52:24 +0200

    init + eslint

--- controllers/routes/api.js
+++ controllers/routes/api.js
@@ -9,18 +9,18 @@ const transportationMeanDb = require('../../database/meeting/transportationMean'
 const MeetingScenario = require('../../model/classes/meeting/MeetingScenario');
 const { meetingCategoryDamage, bounds } = require('../../constants/meeting');
 
-function payloadStructureIsCorrect (){
+function payloadStructureIsCorrect () {
   // TODO check payload structure
-  return true;
+  return false;
 }
 
 app.post('/getDamagesForScenarios', (req, res) => {
-  const scenarios = JSON.parse(req.body.scenarios);
+  const scenarios = req.body;
   if (!payloadStructureIsCorrect(scenarios)) {
-    res.status(400);
-    res.send('Server cannot process the request due to a malformed request syntax.');
+    const errorMessage = { error: 400, message: 'Server cannot process the request due to a malformed request syntax.' };
+    return res.status(400).json(errorMessage);
   } else {
-    damages = scenarios.map(scenario => function (scenario) {
+    const formattedScenarios = scenarios.map(scenario => function (scenario) {
       const damageComputePayload = {
         [meetingCategoryDamage.HARDWARE]: { meetingDuration: scenario.meetingDuration, bound: bounds.UPPER },
         [meetingCategoryDamage.SOFTWARE]: { meetingDuration: scenario.meetingDuration, instancesNumber: scenario.numberOfParticipants, bandwithBound: bounds.UPPER, networkBound: bounds.UPPER },
@@ -29,7 +29,7 @@ app.post('/getDamagesForScenarios', (req, res) => {
       return scenario.computeDamage(damageComputePayload);
     });
 
-    const normalisedDamages = normaliseDamages(scenarios);
+    const normalisedDamages = normaliseDamages(formattedScenarios);
     return res.json(normalisedDamages);
   }
 });
