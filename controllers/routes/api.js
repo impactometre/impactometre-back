5c06e31df76d4304d4fdfc0dde70e5b8ac4d5764
Matt Glorion <matt@glorion.fr>
2020-07-01 22:13:07 +0200

    [ADD] mockup api response

--- controllers/routes/api.js
+++ controllers/routes/api.js
@@ -11,38 +11,113 @@ const MeetingScenario = require('../../model/classes/meeting/MeetingScenario');
 
 function payloadStructureIsCorrect () {
   // TODO check payload structure
-  return true;
+  return false;
 }
 
 app.post('/meeting', async (req, res) => {
   const scenarios = req.body;
   if (!payloadStructureIsCorrect(scenarios)) {
-    const errorMessage = { error: 400, message: 'Bad request. Your request contains bad syntax and cannot be processed.' };
-    return res.status(400).json(errorMessage);
+    const mockupResponse = {
+      comparaison: [
+        [
+          {
+            "damageEndpoint":"HUMAN_HEALTH",
+            "meetingScenario":"6mp34fkc3fqrkm",
+            "value":null,
+            "hardware":null,
+            "software":null,
+            "journey":null
+          },
+          {
+            "damageEndpoint":"HUMAN_HEALTH",
+            "meetingScenario":"6mp34fkc3fqrlf",
+            "value":null,
+            "hardware":null,
+            "software":null,
+            "journey":null
+          }
+        ],
+        [
+          {
+            "damageEndpoint":"ECOSYSTEM_QUALITY",
+            "meetingScenario":"6mp34fkc3fqrkm",
+            "value":null,
+            "hardware":null,
+            "software":null,
+            "journey":null
+          },
+          {
+            "damageEndpoint":"ECOSYSTEM_QUALITY",
+            "meetingScenario":"6mp34fkc3fqrlf",
+            "value":null,
+            "hardware":null,
+            "software":null,
+            "journey":null
+          }
+        ],
+        [
+          {
+            "damageEndpoint":"CLIMATE_CHANGE",
+            "meetingScenario":"6mp34fkc3fqrkm",
+            "value":null,
+            "hardware":null,
+            "software":null,
+            "journey":null
+          },
+          {
+            "damageEndpoint":"CLIMATE_CHANGE",
+            "meetingScenario":"6mp34fkc3fqrlf",
+            "value":null,
+            "hardware":null,
+            "software":null,
+            "journey":null
+          }
+        ],
+        [
+          {
+            "damageEndpoint":"RESOURCES",
+            "meetingScenario":"6mp34fkc3fqrkm",
+            "value":null,
+            "hardware":null,
+            "software":null,
+            "journey":null
+          },
+          {
+            "damageEndpoint":"RESOURCES",
+            "meetingScenario":"6mp34fkc3fqrlf",
+            "value":null,
+            "hardware":null,
+            "software":null,
+            "journey":null
+          }
+        ]
+      ],
+      equivalents: []
+    };
+    return res.json(mockupResponse);
+    // const errorMessage = { error: 400, message: 'Bad request. Your request contains bad syntax and cannot be processed.' };
+    // return res.status(400).json(errorMessage);
   } else {
-    const normalisedDamages = await computeScenarios(scenarios).then(computedScenarios => normaliseDamages(computedScenarios));
+    console.log('INIT');
+    const computedScenarios = await computeScenarios(scenarios);
+    const normalisedDamages = await normaliseDamages(computedScenarios);
     return res.json(normalisedDamages);
   }
 });
 
 async function computeScenarios (scenarios) {
   let computedScenarios = [];
-  console.log(scenarios);
-  for (const scenario of scenarios) {
+  for await (const scenario of scenarios) {
     const s = new MeetingScenario(scenario);
     const computingProperties = {
       hardware: { meetingDuration: scenario.meetingDuration, bounds: 'upper' },
       software: { instancesNumber: scenario.numberOfParticipants, bandwithBound: 'upper', networkBound: 'upper', meetingDuration: scenario.meetingDuration },
       journey: {}
     };
-    s.computeDamage(computingProperties);
-    console.log('SCENARIO COMPUTÉ');
-    console.log(s);
+    const damage = await s.computeDamage(computingProperties);
     computedScenarios.push(s);
   };
-  console.log('-------------------');
-  console.log(computedScenarios);
-  console.log('-------------------');
+
   return Promise.resolve(computedScenarios);
 }
 const { normaliseDamages } = require('../../utils/normalise');
