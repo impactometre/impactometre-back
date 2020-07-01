728dd3f7ef82ab352fcc9eef79e6a9c3ee600e6c
Matt Glorion <matt@glorion.fr>
2020-07-01 23:40:34 +0200

    [UPD] new response body

--- controllers/routes/api.js
+++ controllers/routes/api.js
@@ -17,91 +17,18 @@ function payloadStructureIsCorrect () {
 app.post('/meeting', async (req, res) => {
   const scenarios = req.body;
   if (!payloadStructureIsCorrect(scenarios)) {
-    const mockupResponse = {
-      comparaison: [
-        [
-          {
-            "damageEndpoint":"HUMAN_HEALTH",
-            "meetingScenario":"6mp34fkc3fqrkm",
-            "value":2,
-            "hardware":1,
-            "software":0.5,
-            "journey":0.5
-          },
-          {
-            "damageEndpoint":"HUMAN_HEALTH",
-            "meetingScenario":"6mp34fkc3fqrlf",
-            "value":3,
-            "hardware":1,
-            "software":1,
-            "journey":1
-          }
-        ],
-        [
-          {
-            "damageEndpoint":"ECOSYSTEM_QUALITY",
-            "meetingScenario":"6mp34fkc3fqrkm",
-            "value":4,
-            "hardware":1,
-            "software":2,
-            "journey":1
-          },
-          {
-            "damageEndpoint":"ECOSYSTEM_QUALITY",
-            "meetingScenario":"6mp34fkc3fqrlf",
-            "value":4,
-            "hardware":2,
-            "software":1,
-            "journey":1
-          }
-        ],
-        [
-          {
-            "damageEndpoint":"CLIMATE_CHANGE",
-            "meetingScenario":"6mp34fkc3fqrkm",
-            "value":5,
-            "hardware":0,
-            "software":1,
-            "journey":4
-          },
-          {
-            "damageEndpoint":"CLIMATE_CHANGE",
-            "meetingScenario":"6mp34fkc3fqrlf",
-            "value":4,
-            "hardware":3,
-            "software":0.5,
-            "journey":0.5
-          }
-        ],
-        [
-          {
-            "damageEndpoint":"RESOURCES",
-            "meetingScenario":"6mp34fkc3fqrkm",
-            "value":2,
-            "hardware":0.5,
-            "software":0.5,
-            "journey":1
-          },
-          {
-            "damageEndpoint":"RESOURCES",
-            "meetingScenario":"6mp34fkc3fqrlf",
-            "value":2,
-            "hardware":1,
-            "software":0.5,
-            "journey":0.5
-          }
-        ]
-      ],
-      equivalents: []
-    };
-    return res.json(mockupResponse);
-    // const errorMessage = { error: 400, message: 'Bad request. Your request contains bad syntax and cannot be processed.' };
-    // return res.status(400).json(errorMessage);
+    const errorMessage = { error: 400, message: 'Bad request. Your request contains bad syntax and cannot be processed.' };
+    return res.status(400).json(errorMessage);
   } else {
-    console.log('INIT');
     const computedScenarios = await computeScenarios(scenarios);
     const normalisedDamages = await normaliseDamages(computedScenarios);
-    return res.json(normalisedDamages);
+
+    const responseBody = {
+      comparison: normalisedDamages,
+      equivalents: []
+    };
+
+    return res.json(responseBody);
   }
 });
 
