7fa5e17bea9e29b15893e607cf18115f46c222c7
unknown <v.le-gauche@evea-conseil.com>
2021-04-27 19:10:21 +0200

    docs: add documentation to formatExportService

--- services/meeting/formatExportService.js
+++ services/meeting/formatExportService.js
@@ -21,41 +21,57 @@ const getHeaders = (computedScenarios) => {
   return flattenedHeaders;
 };
 
-const formatDamage = (rawDamage, scenariosValues, category) => {
+/**
+ * For a given category in a scenario and each sphere in rawDamage
+ * replace headers in the scenarioValues array
+ * by the corresponding damage values 
+ * @param {Object} rawDamage The damages of each impact sphere
+ * @param {String} category  The component category
+ * @param {Array} scenarioValues  The scenarioValues array to update
+ * @returns {Array} The scenarioValues array updated
+ */
+const replaceHeadersByDamages = (rawDamage, category, scenarioValues) => {
   Object.entries(rawDamage).forEach(([sphere, damageValue]) => {      
-    const index = scenariosValues.indexOf(`${sphere.replace('_', '')}${category}`);
-    scenariosValues[index] = damageValue;
+    const index = scenarioValues.indexOf(`${sphere.replace('_', '')}${category}`);
+    scenarioValues[index] = damageValue;
   })
 
-  return scenariosValues;
+  return scenarioValues;
 }
 
+/**
+ * Format export data to csv.
+ * @param {Array<Object>} computedScenarios Raw scenarios to format (with damages).
+ * @returns {String} Sceanrios damages in csv format.
+ */
 const formatExportService = (computedScenarios) => {
-
   const headers = getHeaders(computedScenarios);
 
-  const csvData = computedScenarios.map(scenario => {    
+  const csvData = computedScenarios.map(scenario => {
+    // Initialize returned array with headers.
+    // Then, replace each header by its computed damage value.
     let scenarioValues = JSON.parse(JSON.stringify(headers));
 
     Object.entries(scenario._damage).forEach(([category, data]) => {
+      // Total damages part is not formatted like the rest of the object.
       if (category === '_totalDamage') {
-        scenarioValues = formatDamage(data, scenarioValues, category);
+        scenarioValues = replaceHeadersByDamages(data, category, scenarioValues);
 
         return;
       }
       
       const damage = data._totalDamage;
-      scenarioValues = formatDamage(damage, scenarioValues, category);
+      scenarioValues = replaceHeadersByDamages(damage, category, scenarioValues);
     })
 
+    // Add scenario id to the returned array.
     const index = scenarioValues.indexOf(SCENARIO_ID_HEADER);
     scenarioValues[index] = scenario._id;
 
     return scenarioValues;
   });
-  
-  getHeaders(computedScenarios);
 
+  // Transform array to csv-like string.
   let csv = '';
   csv += headers.join(',');
   csv += '\n';
